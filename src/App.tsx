b80c4f055be3685f448f476409967feb8889b05c
sergioeb09 <sergio99es@hotmail.es>
2025-06-26 16:28:21 -0400

    Fix syntax error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import { Header } from './components/Header';
-import StickyNav from './components/StickyNav';
 import { Hero } from './components/Hero';
 import { Story } from './components/Story';
 import { Problem } from './components/Problem';
+import { RealProblem } from './components/RealProblem';
+import { FirstExperience } from './components/FirstExperience';
 import { Solution } from './components/Solution';
 import { Benefits } from './components/Benefits';
 import { Testimonials } from './components/Testimonials';
 import { FAQ } from './components/FAQ';
 import { CallToAction } from './components/CallToAction';
 import { Footer } from './components/Footer';
-import { FirstExperience } from './components/FirstExperience';
-import { RealProblem } from './components/RealProblem';
-export function App() {
-  return <div className="min-h-screen bg-[#FDF9F6] text-[#333333] font-sans overflow-x-hidden w-full">
-      <Header />
-      <StickyNav />
-      <main className="flex-grow overflow-x-hidden w-full">
-        <Hero />
-        <Story />
-        <Problem />
-        <RealProblem />
-        <FirstExperience />
-        <Solution />
-        <Benefits />
-        <Testimonials />
-        <FAQ />
-        <CallToAction />
-      </main>
-      <Footer />
-    </div>;
 
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Story />
+      <Problem />
+      <RealProblem /> {/* <- Your new component */}
+      <FirstExperience />
+      <Solution />
+      <Benefits />
+      <Testimonials />
+      <FAQ />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
